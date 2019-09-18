149d21ac4445d86e7adc1c06e75c4ac67934cb11
Dominic Letz <dominicletz@exosite.com>
2019-09-18 18:32:11 +0200

    Added simple deployment script for Ubuntu 18.04

---
+++ deployment/fabfile.py
@@ -0,0 +1,43 @@
+import os
+import time
+from collections import defaultdict
+from datetime import datetime
+from fabric.api import env, run, cd, put, get, abort, hide, local, lcd, prefix, hosts, sudo
+from fabric.contrib.files import exists
+
+
+env.diode="/opt/diode"
+
+# Install on Ubuntu 18.04
+def install():
+  put(".inputrc", "~")
+
+  # Elixir + Base System
+  if not exists("/usr/bin/elixir"):
+    run("wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb")
+    run("dpkg -i erlang-solutions_1.0_all.deb")
+    run("apt update")
+    run("apt upgrade -y")
+    run("apt install -y screen git snap g++ make autoconf esl-erlang elixir libtool libgmp-dev daemontools cpulimit")
+    run("apt autoremove")
+
+  # Application
+  run("mkdir -p {}".format(env.diode))
+  with cd(env.diode):
+    run("git init")
+    run("git config receive.denyCurrentBranch ignore")
+    local("git push -f ssh://{user}@{host}{path}".format(user=env.user, host=env.host, path=env.diode))
+    run("git checkout master")
+    run("cp githooks/post-receive .git/hooks/")
+    run("cp deployment/diode.service /etc/systemd/system/diode.service")
+    run("HOME=`pwd` mix local.hex --force")
+    run("HOME=`pwd` mix local.rebar --force")
+    run("systemctl daemon-reload")
+
+    # Cleaning
+    run("systemctl stop diode")
+    if exists("./states"):
+      run("find ./states -maxdepth 1 -type f -delete")
+    if exists("./data"):
+      run("find ./data -maxdepth 1 -type f -delete")
+    run("systemctl start diode")
