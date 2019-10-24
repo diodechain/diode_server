4ccdba8f7976ab089fe50a3aaa48d8b8e0189aa7
Dominic Letz <dominicletz@exosite.com>
2019-10-24 13:22:24 +0200

    Added elixir_make

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM elixir:1.9
 
+RUN apt-get update && apt-get install -y libboost-dev
+
 ENV MIX_ENV=prod PORT=8080
 
 COPY mix.* .
 
-RUN mix local.hex --force && mix local.rebar && mix deps.get && mix compile
+RUN mix local.hex --force && mix local.rebar && mix deps.get && mix deps.compile
 
 COPY . .
 
--- mix.exs
+++ mix.exs
@@ -6,7 +6,8 @@ defmodule Diode.Mixfile do
       app: Diode,
       version: "0.0.1",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      compilers: [:elixir_make] ++ Mix.compilers()
     ]
   end
 
@@ -24,7 +25,8 @@ defmodule Diode.Mixfile do
       {:plug, "~> 1.0"},
       {:poison, "~> 3.0"},
       {:libsecp256k1, "~> 0.1.10"},
-      {:keccakf1600, "~> 2.0", hex: :keccakf1600_orig}
+      {:keccakf1600, "~> 2.0", hex: :keccakf1600_orig},
+      {:elixir_make, "~> 0.4", runtime: false}
     ]
   end
 end
