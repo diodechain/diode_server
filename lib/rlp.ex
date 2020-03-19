6461bec2cf2b9fd0991b6b06748e97a7438fd84e
Dominic Letz <dominic@diode.io>
2020-03-19 18:19:59 +0100

    Fixed rlp bug

--- lib/rlp.ex
+++ lib/rlp.ex
@@ -5,8 +5,30 @@ defmodule Rlp do
   @type rlp() :: binary() | [rlp()]
 
   @spec encode!(nil | binary() | maybe_improper_list() | non_neg_integer() | tuple()) :: binary()
-  def encode!(term) do
-    :erlang.iolist_to_binary(do_encode!(term))
+  def encode!(<<x>>) when x < 0x80, do: <<x>>
+
+  def encode!(x) when is_binary(x) do
+    with_length!(0x80, x)
+  end
+
+  def encode!(list) when is_list(list) do
+    with_length!(0xC0, Enum.map(list, &encode!/1))
+  end
+
+  def encode!(other) do
+    encode!(do_encode!(other))
+  end
+
+  defp with_length!(offset, data) do
+    size = :erlang.iolist_size(data)
+
+    if size <= 55 do
+      [offset + size, data]
+    else
+      bin = :binary.encode_unsigned(size)
+      [byte_size(bin) + offset + 55, bin, data]
+    end
+    |> :erlang.iolist_to_binary()
   end
 
   @spec decode!(binary()) :: rlp()
@@ -16,58 +38,23 @@ defmodule Rlp do
   end
 
   defp do_encode!(nil) do
-    do_encode!("")
+    ""
   end
 
   defp do_encode!(struct) when is_struct(struct) do
-    do_encode!(
-      Map.from_struct(struct)
-      |> Enum.map(fn {key, value} -> [Atom.to_string(key), value] end)
-    )
+    Map.from_struct(struct)
+    |> Enum.map(fn {key, value} -> [Atom.to_string(key), value] end)
   end
 
   defp do_encode!(map) when is_map(map) do
-    encode!(
-      Map.to_list(map)
-      |> Enum.map(fn {key, value} ->
-        [if(is_atom(key), do: Atom.to_string(key), else: key), value]
-      end)
-    )
+    Map.to_list(map)
+    |> Enum.map(fn {key, value} ->
+      [if(is_atom(key), do: Atom.to_string(key), else: key), value]
+    end)
   end
 
   defp do_encode!(tuple) when is_tuple(tuple) do
-    encode!(:erlang.tuple_to_list(tuple))
-  end
-
-  defp do_encode!(list) when is_list(list) do
-    list = Enum.map(list, &do_encode!/1)
-    size = :erlang.iolist_size(list)
-
-    head =
-      if size <= 55 do
-        0xC0 + size
-      else
-        bin_length = encode_length(size)
-        [0xF7 + byte_size(bin_length), bin_length]
-      end
-
-    [head, list]
-  end
-
-  defp do_encode!(bin = <<x::unsigned-size(8)>>) when x <= 0x7F do
-    bin
-  end
-
-  defp do_encode!(bin) when is_binary(bin) do
-    head =
-      if byte_size(bin) <= 55 do
-        0x80 + byte_size(bin)
-      else
-        bin_length = encode_length(byte_size(bin))
-        [0xB7 + byte_size(bin_length), bin_length]
-      end
-
-    [head, bin]
+    :erlang.tuple_to_list(tuple)
   end
 
   defp do_encode!(bits) when is_bitstring(bits) do
@@ -77,11 +64,11 @@ defmodule Rlp do
   defp do_encode!(0) do
     # Sucks but this is the quasi standard by Go and Node.js
     # This is why we have bin2num
-    do_encode!("")
+    ""
   end
 
   defp do_encode!(num) when is_integer(num) do
-    do_encode!(:binary.encode_unsigned(num))
+    :binary.encode_unsigned(num)
   end
 
   defp do_decode!(<<x::unsigned-size(8), rest::binary>>) when x <= 0x7F do
@@ -94,7 +81,7 @@ defmodule Rlp do
     {item, rest}
   end
 
-  defp do_decode!(<<head::unsigned-size(8), rest::binary>>) when head <= 0xC0 do
+  defp do_decode!(<<head::unsigned-size(8), rest::binary>>) when head <= 0xBF do
     length_size = (head - 0xB7) * 8
     <<size::unsigned-size(length_size), item::binary-size(size), rest::binary>> = rest
     {item, rest}
@@ -119,15 +106,5 @@ defmodule Rlp do
   defp do_decode_list!(list, rest) do
     {item, rest} = do_decode!(rest)
     do_decode_list!([item | list], rest)
-  end
-
-  defp encode_length(size) do
-    cond do
-      size <= 0xFF ->
-        <<size::unsigned-size(8)>>
-
-      size <= 0xFFFF ->
-        <<size::unsigned-size(16)>>
-    end
   end
 end
