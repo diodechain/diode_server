0fc479c8b1305ac565c20a6cd2678ede876fbcc3
Dominic Letz <dominic@diode.io>
2020-06-01 17:39:03 +0200

    Added missing license headers

--- lib/random.ex
+++ lib/random.ex
@@ -1,3 +1,6 @@
+# Diode Server
+# Copyright 2019 IoT Blockchain Technology Corporation LLC (IBTC)
+# Licensed under the Diode License, Version 1.0
 defmodule Random do
   @moduledoc """
   Random provides random aliases for the range of machine types
