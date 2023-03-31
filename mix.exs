3cbdc75945668a3508dde8279237dac7eff4d3d0
zachdev <zach@diode.io>
2023-03-31 16:16:37 -0500

    fix: update elixir version

--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule Diode.Mixfile do
   def project do
     [
       app: Diode,
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       version: :persistent_term.get(:vsn, @vsn),
       full_version: :persistent_term.get(:full_vsn, @full_vsn),
       source_url: @url,
