7de05fae75e78d583556f9aa5d06a8bfe2fc04d4
Dominic Letz <dominic@diode.io>
2024-07-05 13:13:18 +0200

    Remove deprecated Object.Data test

--- test/object_test.exs
+++ test/object_test.exs
@@ -26,40 +26,4 @@ defmodule ObjectTest do
     assert Server.peer_port(extended) == 2
     assert Server.key(extended) == Wallet.address!(Diode.miner())
   end
-
-  test "encode/decode compat" do
-    objs =
-      for idx <- 1..100 do
-        Object.Data.new(Chain.peak(), "name_#{idx}", "value", Wallet.privkey!(Wallet.new()))
-      end
-
-    for object <- objs do
-      key = Object.key(object)
-
-      encoded = Object.encode!(object)
-      decoded = Object.decode!(encoded)
-
-      assert decoded == object
-      assert Object.key(decoded) == key
-
-      Model.KademliaSql.put_object(Kademlia.hash(key), encoded)
-
-      loaded =
-        Model.KademliaSql.object(Kademlia.hash(key))
-        |> Object.decode!()
-
-      assert Object.key(loaded) == key
-    end
-
-    <<max::integer-size(256)>> = String.duplicate(<<255>>, 32)
-
-    map =
-      Model.KademliaSql.objects(0, max)
-      |> Map.new()
-
-    for object <- objs do
-      key = Kademlia.hash(Object.key(object))
-      assert Object.decode!(Map.get(map, key)) == object
-    end
-  end
 end
