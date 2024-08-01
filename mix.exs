0efea910cc0f373e22bd4bdf8f1338acb9c131b1
Dominic Letz <dominic@diode.io>
2024-08-01 10:26:26 +0200

    v1.5.3

--- mix.exs
+++ mix.exs
@@ -10,8 +10,8 @@ end
 defmodule Diode.Mixfile do
   use Mix.Project
 
-  @vsn "1.5.2"
-  @full_vsn "v1.5.2"
+  @vsn "1.5.3"
+  @full_vsn "v1.5.3"
   @url "https://github.com/diodechain/diode_server"
 
   def project do
